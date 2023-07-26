be7e491a23db74d67e17ec8808f0dec7ffd13268
Dennis N <dupdennis@gmail.com>
2023-07-26 23:09:31 +0300

    Rate limiter

--- 2.js
+++ 2.js
@@ -7,42 +7,36 @@ const router = express.Router();
 
 const store = new MemoryStore();
 
-function bruteforce(namespace, freeRetries, minWait) {
+function rateLimiterMiddleware(namespace, freeRetries, minWait) {
   const bruteforce = new ExpressBrute(store, {
     freeRetries: freeRetries,
     minWait: minWait * 60 * 1000,
+    failCallback: (_req, res, _) => {
+      res
+        .status(429)
+        .send(
+          `Too many requests for the ${namespace} namespace. Please retry in ${minWait} minutes`
+        );
+    },
   });
 
   return (req, res, next) => {
-    bruteforce.prevent(req, res, next, {
-      key: (req) => req.ip + namespace,
-      message: `Too many requests for the ${namespace} namespace. Please retry in ${minWait} minutes`,
-    });
+    bruteforce.prevent(req, res, next);
   };
 }
 
-app.use(bruteforce("global", 2, 5));
+app.use(rateLimiterMiddleware("global", 100, 5));
 
-router.get("/v1/users", bruteforce("users", 50, 1), (req, res) => {
+router.get("/v1/users", rateLimiterMiddleware("users", 50, 1), (req, res) => {
   res.send("This is the /v1/users endpoint.");
 });
 
 router.get("/v1/apps", async (req, res) => {
   try {
-    await new Promise((resolve, reject) => {
-      bruteforce("apps", 2, 2)(req, res, (error) => {
-        if (error) {
-          console.log("bruteforce error", error);
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-    res.send("This is the /v1/apps endpoint.");
-  } catch (error) {
-    console.log("error", error);
-    res.status(429).send(error.message);
+    // promise-based middleware
+    await rateLimiterMiddleware2("apps", 1, 2);
+  } catch (err) {
+    res.error(err.code).send(err.message);
   }
 });
 
